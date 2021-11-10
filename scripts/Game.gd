cf181abd7ba8fd52711fa14e396899c56c989628
djalexkidd <alexpyr@outlook.fr>
2021-11-10 11:01:22 +0100

    yeet

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+var can_shoot = true
 var bullet_ressource = preload("res://scenes/Bullet.tscn")
 
 func _physics_process(delta):
-	if Input.is_action_pressed("click"): #Quand le clic gauche est appuyé
+	if Input.is_action_pressed("click") and can_shoot: #Quand le clic gauche est appuyé
 		var bullet = bullet_ressource.instance() #Instance en tant que noeud
 		add_child(bullet) #L'ajoute dans la scène
+		can_shoot = false
+		$Timer.start()
+
+func _on_Timer_timeout():
+	can_shoot = true
