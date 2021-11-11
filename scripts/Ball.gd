aec54bf9e9073a50bc1451c15f6392733e127278
djalexkidd <alexpyr@outlook.fr>
2021-11-12 00:24:01 +0100

    C'EST ENCORE PÉTÉ

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -6,23 +6,30 @@ const target_y = 5
 const GRAVITY = 5
 
 signal add_score
+signal spawn_small
 
 func _ready():
-	$Strengh.text = String(strengh) #Actualise le compteur de pièces
+	$Strengh.text = String(strengh) #Actualise le compteur
+	connect("add_score", get_node("../HUD"), "_on_Ball_add_score")
+	if is_in_group("ball"):
+		connect("spawn_small", get_node(".."), "_on_Ball_spawn_small")
 
 func _physics_process(delta):
 	velocity.y += GRAVITY
 	move_and_slide(velocity, Vector2.UP)
 	if is_on_wall():
 		velocity.x *= -1
+		$Bounce.play()
 	if is_on_floor():
 		velocity.y *= -1
+		$Bounce.play()
 	
 	if strengh == 0:
+		emit_signal("spawn_small")
 		queue_free()
 
 func _on_Area2D_area_entered(area):
 	if not area.is_in_group("Player"):
 		strengh -= 1
 		emit_signal("add_score")
-		_ready()
+		$Strengh.text = String(strengh) #Actualise le compteur
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var can_shoot = true
 var bullet_ressource = preload("res://scenes/Bullet.tscn")
+var ball_ressource = preload("res://scenes/Ball.tscn")
+var ball_small_ressource = preload("res://scenes/Ball_small.tscn")
+var ball_number = 0
 
 func _physics_process(delta):
 	if Input.is_action_pressed("click") and can_shoot: #Quand le clic gauche est appuyé
@@ -17,3 +20,24 @@ func _on_Player_lose():
 	$BGM.stop()
 	$GameOverSound.play()
 	$HUD/GameOverLabel.show()
+
+func _on_Ball_spawn_small():
+	toast()
+	get_node("ball_small" + str(ball_number)).set_position(Vector2(get_node("ball" + str(ball_number - 1)).position))
+	get_node("ball_small" + str(ball_number)).position.x -= 50
+	toast()
+	get_node("ball_small" + str(ball_number)).set_position(Vector2(get_node("ball" + str(ball_number - 2)).position))
+	get_node("ball_small" + str(ball_number)).position.x += 50
+
+func toast():
+	var ball_small = ball_small_ressource.instance() #Instance en tant que noeud
+	ball_number += 1
+	ball_small.set_name("ball_small" + str(ball_number))
+	add_child(ball_small) #L'ajoute dans la scène
+
+func _on_SpawnNewTimer_timeout():
+	var ball = ball_ressource.instance() #Instance en tant que noeud
+	ball_number += 1
+	ball.set_name("ball" + str(ball_number))
+	add_child(ball) #L'ajoute dans la scène
+	get_node("ball" + str(ball_number)).position = Vector2(640, 320)
