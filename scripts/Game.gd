6a8ab8ca2f328a33908387d270b9c6190be1f754
djalexkidd <alexpyr@outlook.fr>
2022-01-06 20:20:34 +0100

    pew pew

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -28,6 +28,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("click") and can_shoot: #Quand le clic gauche est appuyé
 		var bullet = bullet_ressource.instance() #Instance en tant que noeud
 		add_child(bullet) #L'ajoute dans la scène
+		$ShootSound.play()
 		can_shoot = false
 		$Timer.start()
 
